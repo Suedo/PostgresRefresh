c6c39be799675e5cacfd7c53cd03f9f5d2d73524
snag <somjitnag192@gmail.com>
2020-10-13 10:10:55 +0530

    updated commands yak shaving mistakes

--- used_commands.sh
+++ used_commands.sh
@@ -1,14 +1,31 @@
 # Created a user
-createuser pgnoob -d -s -U pgnoob -w
-
-# Restore tar into dbs
-pg_restore -c -U pgnoob -d northwind -v "resources/northwind.tar"
-pg_restore -c -U pgnoob -d AdventureWorks -v "resources/AdventureWorks.tar"
-pg_restore -c -U pgnoob -d pagila -v "resources/pagila.tar"
-pg_restore -c -U pgnoob -d usda -v "resources/usda.tar"
+createuser pgnoob -d -s
 
 # Create dbs
 createdb northwind -U northwind
-createdb usda -U pgnoob
-createdb AdventureWorks -U pgnoob
-createdb pagila -U pgnoob
+createdb usda -U postgres
+createdb AdventureWorks -U postgres
+createdb pagila -U postgres
+# ^^ I prefer creating through DBeaver:
+#    open a connection (the default postgres one for me)
+#       > right click > create > 
+#           give name of db, select owner (postgres for me)
+# Done
+
+
+# Restore tar into dbs
+pg_restore -c -U postgres -d northwind -v "resources/northwind.tar"
+pg_restore -c -U postgres -d AdventureWorks -v "resources/AdventureWorks.tar"
+pg_restore -c -U postgres -d pagila -v "resources/pagila.tar"
+pg_restore -c -U postgres -d usda -v "resources/usda.tar"
+
+# Drop dbs
+dropdb AdventureWorks -U postgres
+dropdb usda -U postgres
+dropdb pagila -U postgres
+dropdb northwind -U postgres
+
+# currently working on brew installed postgres. 
+# was having problems dropping a db: said other sessions were using them.
+# needed to do `brew services restart postgresql@9.6` , 
+# and waited for appx 10 mins (db to startup properly), and then above commands worked
